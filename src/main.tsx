4979273a3c3527e104fec8a4fc2e57018e650d72
Toby Cm <newtobycm@gmail.com>
2023-10-13 03:26:51 +0000

    maybe no bun anymore?

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// Import styles of packages that you've installed.
+// All packages except `@mantine/hooks` require styles imports
+import "@mantine/core/styles.css";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { MantineProvider } from "@mantine/core";
+import App from "./App";
+
+export default function main() {
+  return (
+    <MantineProvider>
+      <App />
+    </MantineProvider>
+  );
+}
