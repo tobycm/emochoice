eec2f70727f2b965171a64ae18b3c73040a0cb58
Toby <toby@vsus.app>
2023-10-14 20:51:39 -0700

    fix: :fire: het cuu carousel
    Co-authored-by: pdteggman <pdt1806@users.noreply.github.com>

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,19 +1,24 @@
-import { Box, Image } from "@mantine/core";
+import { Carousel } from "@mantine/carousel";
+import { Image } from "@mantine/core";
 import { useEffect, useState } from "react";
 import { getGallery } from "../lib/database";
 
 export default function Gallery() {
-  const [gallery, setGallery] = useState<string[]>([]);
+  const [gallery, setGallery] = useState<string[]>([]); // image links
 
   useEffect(() => {
     getGallery("tobycm").then((gallery) => setGallery(gallery));
   }, []);
 
+  const slides = gallery.map((link) => (
+    <Carousel.Slide key={link}>
+      <Image src={link} />
+    </Carousel.Slide>
+  ));
+
   return (
-    <Box>
-      {gallery.map((link) => (
-        <Image src={link} key={link} />
-      ))}
-    </Box>
+    <Carousel style={{ maxWidth: 500 }} mx="auto" withIndicators height={200}>
+      {slides}
+    </Carousel>
   );
 }
