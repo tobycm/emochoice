4a895da03a4eb16bba08e27980d573f985a0293f
Toby <toby@vsus.app>
2023-10-14 19:46:42 -0700

    feat: :sparkles: make new getGallery function to get gallery images from pocketbase

--- src/lib/database.ts
+++ src/lib/database.ts
@@ -1,5 +1,19 @@
-import PocketBase from "pocketbase";
+import PocketBase, { RecordModel } from "pocketbase";
 
 const pocketbase = new PocketBase("https://pocketbase.emochoice.ca");
 
 export default pocketbase;
+
+export async function getGallery(name: string): Promise<string[]> {
+  const result = await pocketbase
+    .collection("gallery")
+    .getOne<{ pictures: string[] } & RecordModel>("gallery", {
+      filter: `name = ${name}`,
+    });
+
+  const images = result.pictures.map((picture) =>
+    pocketbase.getFileUrl(result, picture)
+  );
+
+  return images;
+}
