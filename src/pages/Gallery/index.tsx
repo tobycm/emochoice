8cd1ea8f49519a025d7ecd820f0e06022c29f2a7
pdt1806 <nmphuc1806@gmail.com>
2023-10-26 20:37:36 -0700

    refactor: :sparkles: add new galleries with fixed ratio 1:1

--- src/pages/Gallery/index.tsx
+++ src/pages/Gallery/index.tsx
@@ -1,5 +1,5 @@
 import { Carousel, Embla, useAnimationOffsetEffect } from "@mantine/carousel";
-import { Box, Image } from "@mantine/core";
+import { Box, Image, Title } from "@mantine/core";
 import { useMediaQuery } from "@mantine/hooks";
 import { useEffect, useState } from "react";
 import { getGallery } from "../../lib/database";
@@ -7,25 +7,33 @@ import { setDocumentTitle } from "../../lib/utils";
 import classes from "./index.module.css";
 
 export default function Gallery() {
-  const [tobyGallery, setTobyGallery] = useState<string[]>([]);
-  const [eggunogallery, setEgguNoGallery] = useState<string[]>([]);
+  const [hoodiesGallery, setHoodiesGallery] = useState<string[]>([]);
+  const [keychainsGallery, setKeychainsNoGallery] = useState<string[]>([]);
+  const [postersGallery, setPostersNoGallery] = useState<string[]>([]);
   const [embla, setEmbla] = useState<Embla | null>(null);
 
   useAnimationOffsetEffect(embla, 750);
 
   useEffect(() => {
     setDocumentTitle("Gallery");
-    getGallery("tobycm").then((gallery) => setTobyGallery(gallery));
-    getGallery("eggu").then((gallery) => setEgguNoGallery(gallery));
+    getGallery("hoodies").then((gallery) => setHoodiesGallery(gallery));
+    getGallery("keychains").then((gallery) => setKeychainsNoGallery(gallery));
+    getGallery("posters").then((gallery) => setPostersNoGallery(gallery));
   }, []);
 
-  const tobySlides = tobyGallery.map((link) => (
+  const hoodiesSlides = hoodiesGallery.map((link) => (
     <Carousel.Slide key={link}>
       <Image src={link} />
     </Carousel.Slide>
   ));
 
-  const eggunoslides = eggunogallery.map((link) => (
+  const keychainsSlides = keychainsGallery.map((link) => (
+    <Carousel.Slide key={link}>
+      <Image src={link} />
+    </Carousel.Slide>
+  ));
+
+  const postersSlides = postersGallery.map((link) => (
     <Carousel.Slide key={link}>
       <Image src={link} />
     </Carousel.Slide>
@@ -33,30 +41,25 @@ export default function Gallery() {
 
   return (
     <Box mb={20} display={"flex"} style={{ flexDirection: "column", alignItems: "center" }} mih="50vh">
-      <Carousel
-        className={classes.carousel}
-        getEmblaApi={setEmbla}
-        mx="auto"
-        loop
-        draggable
-        slideSize={useMediaQuery(`(max-width: 48em)`) ? "100%" : "30%"}
-        slideGap="sm"
-        mb="xl"
-      >
-        {tobySlides}
-      </Carousel>
-      <Carousel
-        className={classes.carousel}
-        getEmblaApi={setEmbla}
-        mx="auto"
-        loop
-        draggable
-        slideSize={useMediaQuery(`(max-width: 48em)`) ? "100%" : "30%"}
-        slideGap="sm"
-        mb="xl"
-      >
-        {eggunoslides}
-      </Carousel>
+      {["Hoodies", "Keychains", "Posters"].map((slides) => (
+        <>
+          <Title order={2} mb="xl" c="emochoice-blue">
+            {slides}
+          </Title>
+          <Carousel
+            className={classes.carousel}
+            getEmblaApi={setEmbla}
+            mx="auto"
+            loop
+            draggable
+            slideSize={useMediaQuery(`(max-width: 48em)`) ? "100%" : "30%"}
+            slideGap="sm"
+            mb="xl"
+          >
+            {slides === "Hoodies" ? hoodiesSlides : slides === "Keychains" ? keychainsSlides : postersSlides}
+          </Carousel>
+        </>
+      ))}
     </Box>
   );
 }
